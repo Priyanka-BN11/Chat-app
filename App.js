afc6e7ba7a6b76d20ba783128f48d6bef0964b2c
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2022-11-10 20:02:27 +0100

    5.2

--- App.js
+++ App.js
@@ -1,47 +1,25 @@
-import React, { Component } from 'react';
-
-// import the screens
-import Start from './components/Start';
-import Chat from './components/Chat';
+import React, { Component } from "react";
+// import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import Chat from "./components/Chat";
+import Start from "./components/Start";
 // import react native gesture handler
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
 // import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-
-export default class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state={
-     
-    }
-  }
-  
-  render() {
-    // Create the navigator
 const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  render() {
     return (
       <NavigationContainer>
-
-<Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
-      </Stack.Navigator>
-    
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
       </NavigationContainer>
-
     );
   }
 }
-
--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+
+import {
+  View,
+  Text,
+  Button,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
   componentDidMount() {
-    // Set the name property to be included in the navigation bar
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+     // Set the name property to be included in the navigation bar
+     let name = this.props.route.params.name;
+     this.props.navigation.setOptions({ title: name });
+     
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: `Hello ${name}, Good Day`,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: `${name} has entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+ //Appends new message to previous
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  //Allows bubble customization   
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: 'white',
+          },
+          right: {
+            backgroundColor: 'green'
+          }
+        }}
+      />
+    );
   }
 
   render() {
-    // Set the color property as background color for the chat screen
-    let color = this.props.route.params.color;
+    
+
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: color,
-        }}
-      >
-        <Text>This is the Chat screen!</Text>
+      <View style={{ flex: 1 }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
